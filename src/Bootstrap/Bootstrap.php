8253958acc1166101b0c890c598be6a4f0018920
Ahmadreza Bashari <ahmadrezabashari@gmail.com>
2025-12-28 17:05:01 +0330

    Added phpdoc to bootstrap and bootstrap interface

--- src/Bootstrap/Bootstrap.php
+++ src/Bootstrap/Bootstrap.php
@@ -5,19 +5,29 @@ namespace Felora\Bootstrap;
 use Closure;
 use Felora\Container\Container;
 use Felora\Contracts\Container\Container as ContainerContract;
+use Felora\Contracts\Foundation\Bootstrap\Bootstrap as BootstrapContract;
 
 /** Bootstraps */
-use Felora\Foundation\Bootstrap\ {
+use Felora\Foundation\Bootstrap\{
     LoadConfig,
     ExceptionHandler,
     InitialRegistration,
 };
-use Felora\Contracts\Foundation\Bootstrap\Bootstrap as BootstrapContract;
 
+/**
+ * Class Bootstrap
+ *
+ * Responsible for loading and executing all defined bootstraps.
+ * Allows registering callbacks before and after each bootstrap execution.
+ *
+ * @package Felora\Bootstrap
+ */
 class Bootstrap extends Container
 {
     /**
-     * @var array<BootstrapContract>
+     * List of bootstrap classes to load.
+     *
+     * @var array<class-string<BootstrapContract>>
      */
     private array $bootstraps = [
         0 => InitialRegistration::class,
@@ -25,48 +35,86 @@ class Bootstrap extends Container
         2 => ExceptionHandler::class,
     ];
 
-    protected null|Closure $bootstrapping = null;
+    /**
+     * Callback executed before each bootstrap class is booted.
+     *
+     * @var null|Closure(string $bootstrapClass)
+     */
+    protected ?Closure $bootstrapping = null;
 
-    protected null|Closure $bootstrapped = null;
+    /**
+     * Callback executed after each bootstrap class has been booted.
+     *
+     * @var null|Closure(BootstrapContract $bootstrapInstance)
+     */
+    protected ?Closure $bootstrapped = null;
 
+    /**
+     * Register a callback to be executed before each bootstrap.
+     *
+     * @param Closure(string $bootstrapClass) $callback
+     */
     public function bootstrapping(Closure $callback): void
     {
         $this->bootstrapping = $callback;
     }
 
+    /**
+     * Register a callback to be executed after each bootstrap.
+     *
+     * @param Closure(BootstrapContract $bootstrapInstance) $callback
+     */
     public function bootstrapped(Closure $callback): void
     {
         $this->bootstrapped = $callback;
     }
 
+    /**
+     * Execute a single bootstrap class immediately.
+     *
+     * @param class-string<BootstrapContract> $bootstrap
+     */
     public function withBootstrap(string $bootstrap): void
     {
         $this->fire($bootstrap);
     }
 
+    /**
+     * Fire the "bootstrapping" callback before executing a bootstrap.
+     *
+     * @param string $bootstrapClass
+     */
     protected function bootstrappingFire(string $bootstrapClass): void
     {
-        if(is_callable($this->bootstrapping))
-        {
+        if (is_callable($this->bootstrapping)) {
             ($this->bootstrapping)($bootstrapClass);
         }
     }
 
+    /**
+     * Fire the "bootstrapped" callback after executing a bootstrap.
+     *
+     * @param BootstrapContract $bootstrap
+     */
     protected function bootstrappedFire(BootstrapContract $bootstrap): void
     {
-        if(is_callable($this->bootstrapped))
-        {
+        if (is_callable($this->bootstrapped)) {
             ($this->bootstrapped)($bootstrap);
         }
     }
 
+    /**
+     * Boot a single bootstrap class, triggering before/after callbacks.
+     *
+     * @param class-string<BootstrapContract> $bootstrap
+     */
     protected function fire(string $bootstrap): void
     {
         $this->bootstrappingFire($bootstrap);
 
-        /** @var BootstrapContract */
-        $instance = (new $bootstrap);
-        /** @var  ContainerContract $container*/
+        /** @var BootstrapContract $instance */
+        $instance = new $bootstrap();
+        /** @var ContainerContract $container */
         $instance->bootstrap($container = $this);
 
         $this->instance($bootstrap, $instance);
@@ -74,10 +122,13 @@ class Bootstrap extends Container
         $this->bootstrappedFire($instance);
     }
 
+    /**
+     * Load and execute all bootstraps in the defined order.
+     */
     protected function loadBootstraps(): void
     {
-        foreach($this->bootstraps as $bootstrap) {
-            $this->fire((string)$bootstrap);
+        foreach ($this->bootstraps as $bootstrap) {
+            $this->fire((string) $bootstrap);
         }
     }
 }
--- src/Contracts/Foundation/Bootstrap/Bootstrap.php
+++ src/Contracts/Foundation/Bootstrap/Bootstrap.php
@@ -4,7 +4,27 @@ namespace Felora\Contracts\Foundation\Bootstrap;
 
 use Felora\Contracts\Container\Container;
 
+/**
+ * Interface Bootstrap
+ *
+ * Represents a framework bootstrapper.
+ *
+ * Each bootstrap class (e.g. LoadConfig, ExceptionHandler, InitialRegistration)
+ * is responsible for bootstrapping a specific part of the framework
+ * and will be executed by the main Bootloader/Bootstrap manager.
+ *
+ * @package Felora\Contracts\Foundation\Bootstrap
+ */
 interface Bootstrap
 {
+    /**
+     * Bootstrap the application.
+     *
+     * This method is called during the bootstrapping phase and receives
+     * the main service container instance.
+     *
+     * @param Container $container
+     * @return void
+     */
     public function bootstrap(Container $container): void;
 }
